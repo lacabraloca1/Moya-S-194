52e6a3192d8cb7313e25ac2f50315502daa8d8d6
CrazyGoat <145066947+lacabraloca1@users.noreply.github.com>
2025-02-20 20:13:02 -0600

    Primera Modficacion

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask , render_template
+import mysql.connector
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="db"
+    )
+    cursor = conn.cursor()
+    cursor.execute("select * from students")
+    students = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html',students=students)
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port=5000,debug=True)
